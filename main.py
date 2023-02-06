af50820d379fd6bf0ab6b5584a0d50d37665e735
fallnomega <fallnomega@gmail.com>
2023-02-06 07:30:27 -0800

    building race

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import turtle as t
 
-# class Turtle_Runner:
-#     def __init__(self,color):
-#         self.Turtle()
-#         self.color(color)
 
+class Turtle_Runner:
+    def __init__(self, color, starting_pos):
+        runner = t.Turtle()
+        runner.shape("turtle")
+        runner.color(color)
+        runner.penup()
+        runner.sety(starting_pos)
+
+
+colors = ('red', 'green', 'blue', 'purple', 'yellow', 'orange')
+position = 0
+for x in range(6):
+    runner1 = Turtle_Runner(colors[x], position)
+    position += 40
 
 screen = t.Screen()
 screen.exitonclick()
-# runner1 = Turtle_Runner("red")
-
-runner_temp = t.Turtle()
-runner_temp.color("red")
-runner_temp.forward(10)
