23fc4ee491216495f6438742ae0e94fc0bd06218
fallnomega <fallnomega@gmail.com>
2023-02-06 10:09:35 -0800

    some code refactor and what not

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# I admit, making a class of a class seems sloppy
+# (class Turtle_Runner using a Turtle object already built
+# into turtle lib). Just wnated to practice some additional
+# class coding just to build the habit where feasible. THis probably
+# wasnt a feasible moment :(
+
+
 import random
 import turtle as t
 
@@ -13,14 +20,15 @@ class Turtle_Runner:
         self.pick = 0
 
     def race(self):
-        distance = random.randint(0,10)
+        distance = random.randint(0, 10)
         self.runner.forward(distance)
 
 
 colors = ('Red', 'Green', 'Blue', 'Purple', 'Yellow', 'Orange')
 screen = t.Screen()
 # screen.setup(width=600,height=500)
-your_bet = screen.textinput('Place your bet', "Pick which turtle will win... choose either Red, Green, Blue, Purple, Yellow, Orange")
+your_bet = screen.textinput('Place your bet',
+                            "Pick which turtle will win... choose either Red, Green, Blue, Purple, Yellow, Orange")
 
 position = 0
 contestants = []
@@ -45,6 +53,5 @@ while contestants[y].runner.xcor() <= 250:
                 print("You win!")
             else:
                 print("You lose!")
-
 
 screen.exitonclick()
